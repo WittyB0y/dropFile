dcabafb1e01b3d1fbd274186e2a9efe52a271bd6
Artem <bobrovniksuper@icloud.com>
2023-02-27 03:26:05 +0300

    v.3.4

--- dropfile/user/models.py
+++ dropfile/user/models.py
@@ -1,2 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class photo(models.Model):
+    userid = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="users/mainphoto/", default="users/mainphoto/user.jpg")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
 
