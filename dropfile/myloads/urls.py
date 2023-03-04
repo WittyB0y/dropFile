ea39943a54ece4f5e5272f06a79aaed41b46ea82
wittyb0y <bobrovniksuper@icloud.com>
2023-03-04 03:58:35 -0500

    v4.1.2

--- dropfile/myloads/urls.py
+++ dropfile/myloads/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
 path('', get_files, name='myFiles'),
+path('delete/<slug:slug>/', delete_file)
 ]
--- dropfile/myloads/views.py
+++ dropfile/myloads/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render, redirect
 from user.models import photo as bd_photo
 from loadFile.models import files as bd_file
+from django.http import HttpResponse
 
 @csrf_protect
 def get_files(request):
@@ -18,3 +19,16 @@ def get_files(request):
             counter = 0
             lst_file = 'Загрузок не было'
     return render(request, 'myloads/myfiles.html', {'photo':photo, 'files':files, 'count':counter, 'lst_file':lst_file})
+
+
+def delete_file(request, slug):
+    if request.user.is_authenticated:
+        try:
+            file = bd_file.objects.get(slug=slug, userid=request.user)
+            file.delete()
+        except:
+            pass
+        finally:
+            return redirect('myFiles')
+    else:
+        return redirect('home')
